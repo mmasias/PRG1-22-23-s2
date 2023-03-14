f072f38160c4009bd409c38ea487bc6d8bc062e4
mmasias@Win11 <manuel@masiasweb.com>
2023-03-14 11:15:44 +0100

    Ejercicios de deteccion de patrones

---
+++ entregas/Prueba.java
@@ -0,0 +1,24 @@
+class Prueba {
+    public static void main(String[] args) {
+
+        int miLongitud = 5;
+        int LONGITUD = miLongitud*3;
+
+        String lineaPorImprimir;
+
+        for (int i = 0; i < LONGITUD; i++) {
+            lineaPorImprimir = "  ".repeat(LONGITUD - i) +  "[]".repeat(i * 2);
+            System.out.println(lineaPorImprimir);
+        }
+
+        for (int i = 0; i < LONGITUD; i++) {
+            if (i < LONGITUD / 2) {
+                lineaPorImprimir ="[]".repeat(LONGITUD * 2);
+            } else {
+                lineaPorImprimir = "[]".repeat(LONGITUD/3*2) + "  ".repeat(LONGITUD/3*2) + "[]".repeat(LONGITUD/3*2);
+            }
+            System.out.println(lineaPorImprimir);
+        }
+        System.out.println("[]".repeat(LONGITUD * 2));
+    }
+}
