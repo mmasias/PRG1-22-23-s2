b43955423e4428568a038396e50daca57a9ab89c
JoseCabrera05 <josecabrerassj2005@gmail.com>
2023-03-28 10:10:54 +0200

    Inciso 20

--- entregas/entregas/retoForIf.java
+++ entregas/entregas/retoForIf.java
@@ -9,7 +9,8 @@ class retoForIf {
             for (int i = 1; i <= dimension; i = i + 1) {
 
                 if (
-                    i %2 == j %2
+                    ( i + j  == 4 || i + j  == 8 ||  i + j  == 12 ||  i + j  == 16 ||  i + j  == 20
+                                    || i + j  == 24 || i + j  == 28 || i + j  == 32 || i + j  == 36 || i + j  == 39 )
                 ) {
                     System.out.print("(*)");
                 } else {
