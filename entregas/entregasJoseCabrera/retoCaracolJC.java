196260fd3ddd8de8dcb7e542710f111e6d78a4f1
JoseCabrera05 <josecabrerassj2005@gmail.com>
2023-05-06 17:24:27 +0200

    Reto Caracol 2.0

--- entregas/entregasJoseCabrera/retoCaracolJC.java
+++ entregas/entregasJoseCabrera/retoCaracolJC.java
@@ -2,69 +2,52 @@ import java.util.Random;
 
 public class retoCaracolJC {
     public static void main(String[] args) {
-        final int PROFUNDIDAD_MINIMA = -20, PROFUNDIDAD_MAXIMA = -10, ALTURA_MINIMA = 0, ALTURA_MAXIMA = 20; 
-        final int MAX_SUBIDA_PRIMEROS_10_DIAS = 4, MAX_SUBIDA_DESPUES_DE_10_DIAS = 3, MAX_SUBIDA_DESPUES_DE_20_DIAS = 2;
-        final int DIAS_MAXIMOS_SIN_SALIR = 50;
-        final double PROBABILIDAD_COCHES = 0.35, PROBABILIDAD_LLUVIA_FUERTE = 0.05, PROBABILIDAD_LLUVIA_MEDIA = 0.1; 
-        final double REDUCCION_PROFUNDIDAD_LLUVIA_FUERTE = 5;
-        final int REDUCCION_PROFUNDIDAD_LLUVIA_MEDIA = 2;
+        int totalDepth = 20;
+        int climbMorning;
+        int fallNight;
+        int position = (int) ((Math.random() * (20 - 10 + 1) + 10));
+        int day = 0;
 
-        char BORDE = '[', PARED = ']', FONDO = ':';
-        String CARACOL = "_@)_/'", AGUA = "~~~~", COCHE = "O-=-O";
+        String upperBorder = "[][]            [][]";
+        String wall = "  [].:.:.:.:.:.:[]  ";
+        String snailWall = "  []   _@)_/’   []  ";
+        String waterWall = "  []~~~~~~~~~~~~[]  ";
+        String lowerBorder = "  [][][][][][][][]  ";
 
-        Random random = new Random();
+        while (position >= 0) {
 
-        int dia;
-        double profundidad;
-        int altura;
-        boolean vivo;
-        int diasSinSalir;
-        int calcularCaida = 0, calcularSubida = 0;
+            day++;
+            climbMorning = (int) (Math.random() * (5 - 1) + 1);
+            fallNight = (int) (Math.random() * (2 - 0) + 1);
 
-        //Caracol
-         {
-            dia = 1;
-            profundidad = PROFUNDIDAD_MINIMA;
-            altura = ALTURA_MINIMA;
-            vivo = true;
-            diasSinSalir = 0;
-        }
-
-        {
-            if (!vivo) {
-                return;
+            if (day >= 0) {
+                position = position - climbMorning + fallNight;
             }
-            
-            if (profundidad >= 0) {
-                System.out.println("¡El caracol ha salido del pozo el día " + dia + "!");
-                return;
+            if (position < 0) {
+                System.out.println("Saliste!");
+            }
+            System.out.println("Posición del caracol: " + position);
+
+            if (day >= 10 && position < totalDepth) {
+                int newPosition;
+                int climbLimit = (int) (Math.random() * (3 - 1) + 1);
+                newPosition = position - climbLimit;
+              ;
             }
 
-            dia++;
-                diasSinSalir++;
-                
-                int subida = calcularSubida;
-                int caida = calcularCaida;
-                
-                altura += subida;
-                altura -= caida;
-                
-                if (altura < 0) {
-                    altura = 0;
-                } else if (altura > ALTURA_MAXIMA) {
-                    altura = ALTURA_MAXIMA;
+            System.out.println(" Dia: [" + day + "] /  Distancia subida: [" + climbMorning + "] / Distancia Bajada: [" + fallNight
+                    + "] / Altura al final del dia: [" + position + "]");
+
+            System.out.println(upperBorder);
+            for (int depth = 0; depth <= totalDepth; depth++) {
+                if (position == depth) {
+                    System.out.println(snailWall + " __ " + depth);
+                } else {
+                    System.out.println(wall + " __ " + depth);
                 }
 
-                if (diasSinSalir >= DIAS_MAXIMOS_SIN_SALIR) {
-                    System.out.println("El caracol ha muerto de inanición después de " + diasSinSalir + " días sin salir del pozo.");
-                    vivo = false;
-                    return;
-                }
-                
-                if (random.nextDouble() < PROBABILIDAD_COCHES) {
-                    System.out.println("Un coche ha aparcado cerca del pozo y ha hecho caer al caracol 2 metros el día " + dia + ".");
-                    altura -= 2;
-                }
+            }
+            System.out.println(lowerBorder);
 
         }
     }
